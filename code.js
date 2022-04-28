6a691a4c7b326d424d40863cb37dafa6789f04ef
yichenkuooo <kuoy571@newschool.edu>
2022-04-28 14:53:40 -0400

    My first commit!

---
+++ code.js
@@ -0,0 +1,51 @@
+var option = [
+'☷ Stay low-key, appreciate nature',
+'☶ High Achievement, Try your best',
+'☵ Express emotion, pay attention to the relationship',
+'☴ You will meet some special to guild you',
+'☳ You will have power, peak performance',
+'☲ Prosper with money, carrer',
+'☱ Good and Bad comes together',
+'☰ Integrity overall success'
+
+];
+
+function Earth() {
+
+  alert('Earth in Ba Gua suggests you to stay low-key, appreciate nature.');
+}
+
+function Mountain() {
+
+  alert('Mountain in Ba Gua suggests trying your best, high achievement is coming.');
+}
+
+function Water() {
+
+  alert('Water in Ba Gua encourages you to express your emotion and pay attention to the relationship.');
+}
+
+function Wind() {
+
+  alert('Wind in Ba Gua suggests you will meet someone special,  maybe a mentor that guild you.');
+}
+
+function Thunder() {
+
+  alert('Thunder in Ba Gua is all about power! it’s a day for your peak performance.');
+}
+
+function Fire() {
+
+  alert('Fire in Ba Gua blesses you with prosperity in money and power.');
+}
+
+function Lake() {
+
+  alert('Lake in Ba Gua reminds you the good and the bad come together. respect the life');
+}
+
+function Heaven() {
+
+  alert('hihi');
+}
